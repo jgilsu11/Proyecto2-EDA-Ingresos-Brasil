fe8c0a1d178c212f71268432990d2e7f6b24f7a8
jgilsu11 <jaimegilnero@gmail.com>
2024-10-11 16:27:39 +0200

    Subida de estructura de carpetas

---
+++ src/Soporte.py
@@ -0,0 +1,1 @@
+#import sys y luego usar sys.path.append("../") y from scr import soporte as sl y donde se quiera meter se pondrá si la funcion sellama sumarr será sl.sumarr y en el archivo jupyter habrá que meter ../ en la ruta del archivo 
