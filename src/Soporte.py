78bd6e2b17aba2566e7e93b6aae220c052ccc144
jgilsu11 <jaimegilnero@gmail.com>
2024-10-11 20:53:41 +0200

    Subida a falta de lo del codigo y nombre y hacer cambios en todos

--- src/Soporte.py
+++ src/Soporte.py
@@ -1,1 +1,4 @@
 #import sys y luego usar sys.path.append("../") y from scr import soporte as sl y donde se quiera meter se pondrá si la funcion sellama sumarr será sl.sumarr y en el archivo jupyter habrá que meter ../ en la ruta del archivo 
+import pandas as pd
+import numpy as np
+from datetime import datetime
